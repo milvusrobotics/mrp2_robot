5f9b3317a539c85aa36d694c4b11a7157c96d2cf
MRP2 Development <mrp2@milvus.com.tr>
2016-01-05 11:54:19 +0200

    Added env var file for installation.

---
+++ mrp2_hardware/install/setup.bash
@@ -0,0 +1,14 @@
+export ROBOT_SETUP=/etc/mrp2/setup.bash
+
+###### 
+export MRP2_IMU=1
+export MRP2_IMU_NAME=um6
+export MRP2_IMU_SERIAL=A4039EMX
+export MRP2_LASER=1
+export MRP2_LASER_NAME=utm30_lx
+export MRP2_SONAR=0
+export MRP2_SONAR_SERIAL=MRP2SNR001
+
+######
+source /opt/ros/indigo/setup.bash
+source /home/mrp/ros_ws/devel/setup.bash
